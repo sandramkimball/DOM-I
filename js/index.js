5681e4c6dd84aeff7304bc3dca458c245dccfae3
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-09-03 13:58:57 -0700

    filling in missing content

--- js/index.js
+++ js/index.js
@@ -37,6 +37,61 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+//header and nav stuff
+const logo = document.getElementById("logo-img");
+logo.src = 'img/logo.png';
+
+const codeSnip = document.querySelector('#cta-img');
+codeSnip.src = 'img/header-img.png';
+
+//nav titles
+const navTitle2 = document.querySelector("nav-item-1")
+navTitle2.textContent = "Services";
+
+const navTitle3 = document.querySelector('nav-item-2');
+navTitle3.textContent = "Product";
+
+const navTitle4 = document.querySelector('nav-item-3');
+navTitle.textContent = "Vision";
+
+const navTitle5 = document.querySelector('nav-item-4');
+navTitle5.textContent = "Features";
+
+const navTitle6 = document.querySelector('nav-item-5');
+navTitle6.textContent = "About";
+
+const navTitle7 = document.querySelector('nav-item-6');
+navTitle7.textContent = "Contact";
+
+
+const navigationColor = document.querySelector('header');
+navigationColor.style.backgroundColor = "seagreen";
+
+
+const newNav1 = document.createElement('a');
+newNav1.textContent = "Peanut Butter";
+"Peanut Butter"
+header.appendChild(newNav1);
+
+const fullNavigation = document.querySelector('header nav');
+fullNavigation.appendChild(newNav1);
+
+
+const newNav2 = document.createElement('h1');
+newNav2.textContent = "Jelly Time!";
+"Jelly Time!"
+fullNavigation.prepend(newNav2);
+
+
+
+//cta thing
+
+const domAwesome = document.querySelector('.cta .cta-text  h1');
+domAwesome.textContent =  "Dom Is Awesome";
+
+const startedButton = document.querySelector('.cta .cta-text button');
+startedButton.textContent =  "Get Started";
+
+//middle body
+const middleImg = document.querySelector('#middle-img');
+middleImg.src = 'img/mid-page-accent.jpg';
